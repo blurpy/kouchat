b4eb72f61161744947bb2f20a7ae054da467d26b
Christian Ihle <blurpy@gmail.com>
2007-11-09 19:15:09 +0000

    Focus fix.

--- java/kouchat/src/net/usikkert/kouchat/ui/swing/SettingsDialog.java
+++ java/kouchat/src/net/usikkert/kouchat/ui/swing/SettingsDialog.java
@@ -28,6 +28,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -143,6 +145,15 @@ public class SettingsDialog extends JDialog implements ActionListener
 		getRootPane().setDefaultButton( saveB );
 		
 		settings = Settings.getSettings();
+		
+		addWindowListener( new WindowAdapter()
+		{
+			@Override
+			public void windowActivated( WindowEvent e )
+			{
+				nickTF.requestFocusInWindow();
+			}
+		} );
 	}
 	
 	public void setMediator( Mediator mediator )
