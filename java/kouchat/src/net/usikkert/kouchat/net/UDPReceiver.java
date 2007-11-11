5219b2a4f0db596ae82dc3d7caff3b3acd4f5cf6
Christian Ihle <blurpy@gmail.com>
2007-11-11 13:01:47 +0000

    Improved error when all udp ports are used.

--- java/kouchat/src/net/usikkert/kouchat/net/UDPReceiver.java
+++ java/kouchat/src/net/usikkert/kouchat/net/UDPReceiver.java
@@ -123,8 +123,13 @@ public class UDPReceiver implements Runnable
 
 		if ( !done )
 		{
-			errorHandler.showError( "Failed to initialize udp network:\n" +
-					"\n\nYou will not be able to receive private messages!" );
+			String error = "Failed to initialize udp network:" +
+					"\nNo available listening port between " + Constants.NETWORK_PRIVCHAT_PORT +
+					" and " + ( port -1 ) + "." +
+					"\n\nYou will not be able to receive private messages!";
+			
+			log.log( Level.SEVERE, error );
+			errorHandler.showError( error );
 		}
 	}
 
