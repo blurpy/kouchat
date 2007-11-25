92229357f3c65209951b851640b7a36f2a051997
Christian Ihle <blurpy@gmail.com>
2007-11-25 17:51:11 +0000

    Release 0.9.4

--- java/kouchat/src/net/usikkert/kouchat/Constants.java
+++ java/kouchat/src/net/usikkert/kouchat/Constants.java
@@ -36,7 +36,7 @@ public interface Constants
 	/**
 	 * The application version.
 	 */
-	public static final String APP_VERSION = "0.9.4-dev";
+	public static final String APP_VERSION = "0.9.4";
 
 	/**
 	 * Which license the application has.
