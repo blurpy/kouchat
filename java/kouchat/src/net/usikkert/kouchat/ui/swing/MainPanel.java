0cd9bd6b940ba395b522608dc7bc8861b9a01088
Christian Ihle <blurpy@gmail.com>
2007-12-15 14:28:05 +0000

    Tweaked borders

--- java/kouchat/src/net/usikkert/kouchat/ui/swing/MainPanel.java
+++ java/kouchat/src/net/usikkert/kouchat/ui/swing/MainPanel.java
@@ -99,7 +99,7 @@ public class MainPanel extends JPanel implements ActionListener, CaretListener, 
 		new MsgPopup( msgTF );
 		new ChatPopup( chatTP );
 
-		setBorder( BorderFactory.createEmptyBorder( 5, 5, 5, 5 ) );
+		setBorder( BorderFactory.createEmptyBorder( 2, 2, 2, 2 ) );
 		cmdHistory = new CommandHistory();
 	}
 
