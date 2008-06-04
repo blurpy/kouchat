2204eb4f512704981733e9055af2a14def5979d0
Christian Ihle <blurpy@gmail.com>
2008-06-04 19:43:11 +0000

    Added a simple input validator.

---
+++ java/kouchat/src/net/usikkert/kouchat/util/Validate.java
@@ -0,0 +1,51 @@
+
+/***************************************************************************
+ *   Copyright 2006-2008 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.util;
+
+/**
+ * Contains utility methods for validating input.
+ *
+ * @author Christian Ihle
+ */
+public final class Validate
+{
+	/**
+	 * Private constructor. Only static methods in this class.
+	 */
+	private Validate()
+	{
+
+	}
+
+	/**
+	 * Checks if <code>obj</code> is <code>null</code>, and throws
+	 * an {@link IllegalArgumentException} if that is true.
+	 *
+	 * @param obj The object to check.
+	 * @param errorMsg The error message to use in the exception if the check returns false.
+	 */
+	public static void notNull( final Object obj, final String errorMsg )
+	{
+		if ( obj == null )
+			throw new IllegalArgumentException( errorMsg );
+	}
+}
