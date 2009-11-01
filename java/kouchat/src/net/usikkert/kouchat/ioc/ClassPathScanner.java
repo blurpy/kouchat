4582d519241c7df256c5b1c93c9e2f3be065a26d
Christian Ihle <blurpy@gmail.com>
2009-11-01 14:01:45 +0000

    Adding simple ioc container

---
+++ java/kouchat/src/net/usikkert/kouchat/ioc/ClassPathScanner.java
@@ -0,0 +1,181 @@
+
+/***************************************************************************
+ *   Copyright 2006-2009 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.ioc;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+import java.util.logging.Logger;
+
+import net.usikkert.kouchat.util.Tools;
+
+/**
+ *
+ * @author Christian Ihle
+ */
+public class ClassPathScanner
+{
+	private static final Logger LOG = Logger.getLogger( ClassPathScanner.class.getName() );
+
+	public Set<Class<?>> findClasses( final String packageName )
+	{
+		final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+
+		try
+		{
+			final long start = System.currentTimeMillis();
+			final Set<Class<?>> classes = findClasses( loader, packageName );
+			final long stop = System.currentTimeMillis();
+
+			LOG.info( "Time spent scanning classpath: " + ( stop - start ) + " ms" );
+			LOG.info( "Classes found: " + classes.size() );
+
+			return classes;
+		}
+
+		catch ( final Exception e )
+		{
+			throw new RuntimeException( e );
+		}
+	}
+
+	private Set<Class<?>> findClasses( final ClassLoader loader, final String packageName ) throws Exception
+	{
+		final Set<Class<?>> classes = new HashSet<Class<?>>();
+		final String path = packageName.replace( '.', '/' );
+		final Enumeration<URL> resources = loader.getResources( path );
+
+		if ( resources != null )
+		{
+			while ( resources.hasMoreElements() )
+			{
+				String filePath = resources.nextElement().getFile();
+				// WINDOWS HACK
+				if ( filePath.indexOf( "%20" ) > 0 )
+				{
+					filePath = filePath.replaceAll( "%20", " " );
+				}
+
+				if ( filePath != null )
+				{
+					if ( ( filePath.indexOf( "!" ) > 0 ) && ( filePath.indexOf( ".jar" ) > 0 ) )
+					{
+						String jarPath = filePath.substring( 0, filePath.indexOf( "!" ) )
+								.substring( filePath.indexOf( ":" ) + 1 );
+
+						// WINDOWS HACK
+						if ( jarPath.indexOf( ":" ) >= 0 )
+						{
+							jarPath = jarPath.substring( 1 );
+						}
+
+						classes.addAll( getFromJARFile( jarPath, path ) );
+					}
+
+					else
+					{
+						classes.addAll( getFromDirectory( new File( filePath ), packageName ) );
+					}
+				}
+			}
+		}
+
+		return classes;
+	}
+
+	private Set<Class<?>> getFromDirectory( final File directory, final String packageName ) throws Exception
+	{
+		final Set<Class<?>> classes = new HashSet<Class<?>>();
+
+		if ( directory.exists() )
+		{
+			final File[] files = directory.listFiles();
+
+			for ( final File file : files )
+			{
+				if ( file.isDirectory() )
+				{
+					classes.addAll( getFromDirectory( file, packageName + "." + file.getName() ) );
+				}
+
+				else if ( file.getName().endsWith( ".class" ) )
+				{
+					final String name = packageName + '.' + stripFilenameExtension( file.getName() );
+					final Class<?> clazz = Class.forName( name );
+					addClass( clazz, classes );
+				}
+			}
+		}
+
+		return classes;
+	}
+
+	private static String stripFilenameExtension( final String file )
+	{
+		return Tools.getFileBaseName( file );
+	}
+
+	private Set<Class<?>> getFromJARFile( final String jar, final String packageName ) throws Exception
+	{
+		final Set<Class<?>> classes = new HashSet<Class<?>>();
+		final JarInputStream jarFile = new JarInputStream( new FileInputStream( jar ) );
+		JarEntry jarEntry;
+
+		do
+		{
+			jarEntry = jarFile.getNextJarEntry();
+
+			if ( jarEntry != null )
+			{
+				String className = jarEntry.getName();
+
+				if ( className.endsWith( ".class" ) )
+				{
+					className = stripFilenameExtension( className );
+
+					if ( className.startsWith( packageName ) )
+					{
+						final Class<?> clazz = Class.forName( className.replace( '/', '.' ) );
+						addClass( clazz, classes );
+
+					}
+				}
+			}
+		} while ( jarEntry != null );
+
+		return classes;
+	}
+
+	private void addClass( final Class<?> clazz, final Set<Class<?>> classes )
+	{
+		if ( !clazz.isAnonymousClass() && !clazz.isMemberClass() && !clazz.isSynthetic()
+				&& !clazz.isAnnotation() && !clazz.isEnum() && !clazz.isInterface() )
+		{
+			classes.add( clazz );
+		}
+	}
+}
---
+++ java/kouchat/src/net/usikkert/kouchat/ioc/DefaultAnnotatedBeanLoader.java
@@ -0,0 +1,175 @@
+
+/***************************************************************************
+ *   Copyright 2006-2009 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.ioc;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Christian Ihle
+ */
+public class DefaultAnnotatedBeanLoader implements BeanLoader
+{
+	private static final Logger LOG = Logger.getLogger( DefaultAnnotatedBeanLoader.class.getName() );
+
+	private static final String BASE_PACKAGE = "net.usikkert.kouchat";
+
+	private final Map<Class<?>, Object> beans = new HashMap<Class<?>, Object>();
+
+	@Override
+	public void loadBeans()
+	{
+		try
+		{
+			loadAndAutowireBeans();
+		}
+
+		catch ( final Exception e )
+		{
+			throw new RuntimeException( e );
+		}
+	}
+
+	@Override
+	public void autowire( final Object objectToAutowire )
+	{
+		try
+		{
+			autowire( objectToAutowire.getClass(), objectToAutowire );
+		}
+
+		catch ( final Exception e )
+		{
+			throw new RuntimeException( e );
+		}
+	}
+
+	private void loadAndAutowireBeans() throws Exception
+	{
+		final Set<Class<?>> detectedBeans = findBeans( Bean.class );
+
+		for ( final Class<?> beanClass : detectedBeans )
+		{
+			LOG.info( "Bean found: " + beanClass.getName() );
+			final Object bean = instantiateBean( beanClass );
+			beans.put( beanClass, bean );
+		}
+
+		autowireDetectedBeans();
+	}
+
+	private Object instantiateBean( final Class<?> beanClass ) throws Exception
+	{
+		final Constructor<?> constructor = beanClass.getDeclaredConstructor();
+		final boolean originalAccessible = constructor.isAccessible();
+		constructor.setAccessible( true );
+		final Object newInstance = constructor.newInstance();
+		constructor.setAccessible( originalAccessible );
+
+		return newInstance;
+	}
+
+	private Set<Class<?>> findBeans( final Class<? extends Annotation> annotation )
+	{
+		final ClassPathScanner classPathScanner = new ClassPathScanner();
+		final Set<Class<?>> allClasses = classPathScanner.findClasses( BASE_PACKAGE );
+		final Set<Class<?>> detectedBeans = new HashSet<Class<?>>();
+
+		for ( final Class<?> clazz : allClasses )
+		{
+			if ( clazz.isAnnotationPresent( annotation ) )
+			{
+				detectedBeans.add( clazz );
+			}
+		}
+
+		return detectedBeans;
+	}
+
+	private void autowireDetectedBeans() throws Exception
+	{
+		final Iterator<Class<?>> beanIterator = beans.keySet().iterator();
+
+		while ( beanIterator.hasNext() )
+		{
+			final Class<?> beanClass = beanIterator.next();
+			autowire( beanClass, beans.get(beanClass) );
+		}
+	}
+
+	private void autowire( final Class<?> classToAutowire, final Object objectToAutowire ) throws Exception
+	{
+		final Field[] fields = classToAutowire.getDeclaredFields();
+
+		for ( final Field field : fields )
+		{
+			if ( field.isAnnotationPresent( Inject.class ) )
+			{
+				LOG.info( "Field detected for autowiring: " + field );
+				final boolean originalAccessible = field.isAccessible();
+				field.setAccessible( true );
+				final Object bean = findBean( field );
+				field.set( objectToAutowire, bean );
+				field.setAccessible( originalAccessible );
+			}
+		}
+	}
+
+	private Object findBean( final Field field )
+	{
+		final Class<?> beanNeeded = field.getType();
+		final Iterator<Class<?>> beanIterator = beans.keySet().iterator();
+		final List<Object> matches = new ArrayList<Object>();
+
+		while ( beanIterator.hasNext() )
+		{
+			final Class<?> beanClass = beanIterator.next();
+
+			if ( beanNeeded.isAssignableFrom( beanClass ) )
+			{
+				matches.add( beans.get( beanClass ) );
+			}
+		}
+
+		if ( matches.size() == 0 )
+		{
+			throw new RuntimeException( "No matching bean found for autowiring " + beanNeeded );
+		}
+
+		else if ( matches.size() > 1 )
+		{
+			throw new RuntimeException( "Wrong number of beans found for autowiring " + beanNeeded + " " + matches );
+		}
+
+		return matches.get( 0 );
+	}
+}
