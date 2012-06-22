6e21337791b552c8cd2caf4e39af8de348612f85
Christian Ihle <blurpy@gmail.com>
2012-06-22 21:38:20 +0200

    Changing version to 1.2.0

--- src/main/java/net/usikkert/kouchat/Constants.java
+++ src/main/java/net/usikkert/kouchat/Constants.java
@@ -37,7 +37,7 @@ public interface Constants {
     /**
      * The application version.
      */
-    String APP_VERSION = "1.1.0";
+    String APP_VERSION = "1.2.0";
 
     /**
      * Which license the application has.
