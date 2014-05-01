8931b0f90c784cedabe167447869c99e84c927b4
Christian Ihle <blurpy@gmail.com>
2014-05-01 11:36:48 +0200

    Adding more realistic tests of settingslisteners to prepare for refactoring

--- src/test/java/net/usikkert/kouchat/misc/SettingsTest.java
+++ src/test/java/net/usikkert/kouchat/misc/SettingsTest.java
@@ -25,6 +25,7 @@ package net.usikkert.kouchat.misc;
 import static org.junit.Assert.*;
 
 import net.usikkert.kouchat.Constants;
+import net.usikkert.kouchat.event.SettingsListener;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -38,10 +39,25 @@ public class SettingsTest {
 
     private Settings settings;
 
+    private String lastChangedSetting;
+
     @Before
     public void setUp() throws Exception {
         settings = new Settings();
+
+        // Setting these to known values to avoid tests behaving differently on different machines
+        settings.setSound(false);
+        settings.setLogging(false);
+        settings.setOwnColor(0);
+        settings.setSysColor(0);
+
         System.setProperty("file.separator", "/");
+
+        settings.addSettingsListener(new SettingsListener() {
+            public void settingChanged(final String setting) {
+                lastChangedSetting = setting;
+            }
+        });
     }
 
     @Test
@@ -63,5 +79,85 @@ public class SettingsTest {
         settings.setLogLocation(null);
 
         assertEquals(Constants.APP_LOG_FOLDER, settings.getLogLocation());
+    }
+
+    @Test
+    public void setLoggingShouldNotNotifyListenersIfSettingIsUnchanged() {
+        assertFalse(settings.isLogging());
+
+        settings.setLogging(false);
+
+        assertFalse(settings.isLogging());
+        assertNull(lastChangedSetting);
+    }
+
+    @Test
+    public void setLoggingShouldNotifyListenersIfSettingIsChanged() {
+        assertFalse(settings.isLogging());
+
+        settings.setLogging(true);
+
+        assertTrue(settings.isLogging());
+        assertEquals("logging", lastChangedSetting);
+    }
+
+    @Test
+    public void setSoundShouldNotNotifyListenersIfSettingIsUnchanged() {
+        assertFalse(settings.isSound());
+
+        settings.setSound(false);
+
+        assertFalse(settings.isSound());
+        assertNull(lastChangedSetting);
+    }
+
+    @Test
+    public void setSoundShouldNotifyListenersIfSettingIsChanged() {
+        assertFalse(settings.isSound());
+
+        settings.setSound(true);
+
+        assertTrue(settings.isSound());
+        assertEquals("sound", lastChangedSetting);
+    }
+
+    @Test
+    public void setOwnColorShouldNotNotifyListenersIfSettingIsUnchanged() {
+        assertEquals(0, settings.getOwnColor());
+
+        settings.setOwnColor(0);
+
+        assertEquals(0, settings.getOwnColor());
+        assertNull(lastChangedSetting);
+    }
+
+    @Test
+    public void setOwnColorShouldNotifyListenersIfSettingIsChanged() {
+        assertEquals(0, settings.getOwnColor());
+
+        settings.setOwnColor(100);
+
+        assertEquals(100, settings.getOwnColor());
+        assertEquals("ownColor", lastChangedSetting);
+    }
+
+    @Test
+    public void setSysColorShouldNotNotifyListenersIfSettingIsUnchanged() {
+        assertEquals(0, settings.getSysColor());
+
+        settings.setSysColor(0);
+
+        assertEquals(0, settings.getSysColor());
+        assertNull(lastChangedSetting);
+    }
+
+    @Test
+    public void setSysColorShouldNotifyListenersIfSettingIsChanged() {
+        assertEquals(0, settings.getSysColor());
+
+        settings.setSysColor(100);
+
+        assertEquals(100, settings.getSysColor());
+        assertEquals("sysColor", lastChangedSetting);
     }
 }
