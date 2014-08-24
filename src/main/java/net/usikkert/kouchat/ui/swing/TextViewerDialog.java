424a936f4ce993e5b210899ce1b9088bf2fff30f
Christian Ihle <blurpy@gmail.com>
2014-08-24 14:27:14 +0200

    Renaming imageloader and textviewer message properties

--- src/main/java/net/usikkert/kouchat/ui/swing/TextViewerDialog.java
+++ src/main/java/net/usikkert/kouchat/ui/swing/TextViewerDialog.java
@@ -167,7 +167,7 @@ public class TextViewerDialog extends JDialog {
         }
 
         else {
-            errorHandler.showError(messages.getMessage("swing.textViewerDialog.error.openFile", textFile));
+            errorHandler.showError(messages.getMessage("swing.textViewerDialog.errorPopup.openFile", textFile));
         }
     }
 
