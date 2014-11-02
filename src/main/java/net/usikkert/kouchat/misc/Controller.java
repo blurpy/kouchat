259a2cb36365d895a92a7a13f0c3ab9a10c820f3
Christian Ihle <blurpy@gmail.com>
2014-11-02 18:22:18 +0100

    Changing message keys

--- src/main/java/net/usikkert/kouchat/misc/Controller.java
+++ src/main/java/net/usikkert/kouchat/misc/Controller.java
@@ -148,10 +148,10 @@ public class Controller implements NetworkConnectionListener {
         dayTimer.startTimer();
         idleThread.start();
 
-        msgController.showSystemMessage(coreMessages.getMessage("core.startup.welcome.systemMessage",
+        msgController.showSystemMessage(coreMessages.getMessage("core.startup.systemMessage.welcome",
                                                                 Constants.APP_NAME, Constants.APP_VERSION));
         final String date = dateTools.currentDateToString(coreMessages.getMessage("core.dateFormat.today"));
-        msgController.showSystemMessage(coreMessages.getMessage("core.startup.todayIs.systemMessage", date));
+        msgController.showSystemMessage(coreMessages.getMessage("core.startup.systemMessage.todayIs", date));
     }
 
     /**
